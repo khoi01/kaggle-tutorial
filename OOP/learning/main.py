17c5531726dade8b15b7a0fc461efb9ce1039bdb
Abdul Qayyum Isahak <khoi00091@gmail.com>
2025-04-11 11:43:38 +0800

    1. What is OOP? Real-World Analogy
    1. What is OOP? Real-World Analogy

--- OOP/learning/main.py
+++ OOP/learning/main.py
@@ -1,9 +1,27 @@
-# Entry point for OOP tutorial
+class Car:
+    def __init__(self,brand,color):
+        self.brand = brand
+        self.color = color
+        self.fuel = 100 #full tank
+
+    def drive(self):
+        if self.fuel > 0:
+            self.fuel -= 10
+            print(f"{self.brand} is driving.Fuel left: {self.fuel}")
+        else:
+            print(f"{self.brand} can't drive. Out of fuel!")
+    
+    def refuel(self):
+        self.fuel = 100
+        print(f"{self.brand} refueled to full tank.")
+
+
 
 def main():
-    print("🧠 Python OOP Tutorial Starting...")
-    print("Let's learn Classes, Objects, and more!")
-    print("hellow world!")
+    car1 = Car("Honda","Green")
+    for call in range(11):
+        car1.drive()
+
 
 if __name__ == '__main__':
     main()
