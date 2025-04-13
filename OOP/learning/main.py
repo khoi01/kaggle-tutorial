5cf7d3600e46aa3fc2980d415b5e7af76b8db74b
Abdul Qayyum Isahak <khoi00091@gmail.com>
2025-04-13 23:14:33 +0800

    11. super() and Parent Access in Python OOP
    11. super() and Parent Access in Python OOP

--- OOP/learning/main.py
+++ OOP/learning/main.py
@@ -1,3 +1,20 @@
+class Employee:
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+    
+    def describe(self):
+        return f"{self.name} earn ${self.salary}"
+
+class Manager(Employee):
+    def __init__(self, name, salary,department):
+        super().__init__(name, salary)
+        self.department = department
+
+    def describe(self):
+        base =  super().describe() # call parent function
+        return f"{base} They manage the {self.department} department."
+
 # Parent class
 class Animal:
     def __init__(self, name):
@@ -14,16 +31,19 @@ class Dog(Animal):
 # Another child class
 class Cat(Animal):
     def speak(self):
-        return super().speak()
+        return super().speak() 
 
 
 def main():
-    # Create objects
-    dog = Dog("Buddy")
-    cat = Cat("Luna")
+    manager =Manager("khoi",20000,"IT")
+    print(manager.describe())
 
-    print(dog.speak())  # Buddy says Woof!
-    print(cat.speak())  # Luna says Meow!
+    # # Create objects
+    # dog = Dog("Buddy")
+    # cat = Cat("Luna")
+
+    # print(dog.speak())  # Buddy says Woof!
+    # print(cat.speak())  # Luna says Meow!
 
 
 
