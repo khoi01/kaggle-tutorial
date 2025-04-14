d4930c9f72099ac34986b3973c88641622854b5a
Abdul Qayyum Isahak <khoi00091@gmail.com>
2025-04-14 21:30:33 +0800

    20. Interfaces and Custom Exceptions
    20. Interfaces and Custom Exceptions

--- OOP/learning/main.py
+++ OOP/learning/main.py
@@ -2,6 +2,34 @@
 
 from abc import ABC, abstractmethod
 
+class PaymentMethod(ABC):
+    
+    @abstractmethod
+    def pay(self,amount:float):
+        pass
+    
+class CreditCard(PaymentMethod):
+    def pay(self, amount:float):
+        return f"Paid ${amount} with Credit Card"
+
+class Paypal(PaymentMethod):
+    def pay(self, amount:float):
+        return f"Paid ${amount} via PayPal"
+
+class PaymentFailedError(Exception):
+    """Raised when payment could not be completed."""
+    def __init__(self,reason:float):
+        super().__init__(f"Payment failed: {reason}")
+        self.reason = reason
+
+
+def process_payment(payment_method,amount):
+    if(amount<=0):
+        raise PaymentFailedError("Invalid payment amount.")
+    print(payment_method.pay(amount))
+    
+    
+    
 
 class Exported(ABC):
     @abstractmethod
@@ -23,11 +51,19 @@ def run_export(exported:Exported,data:str):
 
 
 def main():
-    pdf = PDFExporter()
-    csv = CSVExporter()
     
-    run_export(pdf,"Report")
-    run_export(csv,"Invoice")
+    try:
+        paypal = Paypal()
+        process_payment(paypal,200)
+        process_payment(paypal,-20)
+    except PaymentFailedError as e:
+        print("!",e)
+    
+    # pdf = PDFExporter()
+    # csv = CSVExporter()
+    
+    # run_export(pdf,"Report")
+    # run_export(csv,"Invoice")
 
 
 
