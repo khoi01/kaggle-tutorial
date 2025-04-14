49b97f4eebffba5631845f30c69af240895c8f00
Abdul Qayyum Isahak <khoi00091@gmail.com>
2025-04-14 23:49:01 +0800

    | 3 | 🔄 Returning JSON responses | Use `jsonify()` to return clean, structured API responses |
    | 3 | 🔄 Returning JSON responses | Use `jsonify()` to return clean, structured API responses |

---
+++ Flask/tutorial_flask/topic_3/app.py
@@ -0,0 +1,48 @@
+# app.py
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+#GET
+@app.route('/status')
+def status():
+    response = {
+        "code":200,
+        "success": True,
+        "message": "API is working",
+        "data": None
+    }
+    #return jsonify(response)
+    return response
+
+#GET
+@app.route("/")
+def home():
+    return "Hello, Flask from Docker! 🚀"
+    
+#GET
+@app.route("/greet")
+def greet():
+    name = request.args.get("name","Guest")
+    return f"Hello, {name}!"
+
+#POST
+@app.route("/message",methods=["POST"])
+def message():
+    data = request.get_json()
+    return jsonify({
+        "received": data,
+        "status": "message received."
+    })
+
+#DELETE
+@app.route("/delete/<item>",methods=["DELETE"])
+def delete_item(item):
+      return jsonify({
+        "item_deleted": item,
+        "status": "Deleted 🗑️"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
