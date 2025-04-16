75dcec0b3994f0bded8a72973d25ddd1c3ac0679
Abdul Qayyum Isahak <khoi00091@gmail.com>
2025-04-16 13:24:38 +0800

    8.1 📁 Applying Repository Pattern with Async + Simulated Delay & Replace from Flask to Quart
    8.1 📁 Applying Repository Pattern with Async + Simulated Delay & Replace from Flask to Quart

--- Flask/tutorial_flask/topic_8.1/app.py
+++ Flask/tutorial_flask/topic_8.1/app.py
@@ -1,11 +1,13 @@
 # app.py
 import os
 
-from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify
+from quart import Quart
 from dotenv import load_dotenv
 from routes.posts import posts_bp
 from routes.gets import gets_bp
 from routes.deletes import deletes_bp
+from routes.user_routes import user_bp
 
 #Load variables from .env file
 load_dotenv()
@@ -13,7 +15,7 @@ load_dotenv()
 def create_app():
     
 
-    app = Flask(__name__)
+    app = Quart(__name__)
 
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     app.config["UPLOAD_DIR"] = os.getenv("UPLOAD_DIR","uploads") #default
@@ -21,11 +23,11 @@ def create_app():
     app.register_blueprint(posts_bp,url_prefix="/posts")
     app.register_blueprint(gets_bp,url_prefix="/gets")
     app.register_blueprint(deletes_bp,url_prefix="/deletes")
-
+    app.register_blueprint(user_bp,url_prefix="/users")
         
     return app
       
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', use_reloader=False)
