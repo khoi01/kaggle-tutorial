fd8f0872ab6a75500d76e70ebca6f58aba75e32f
Abdul Qayyum Isahak <khoi00091@gmail.com>
2025-04-17 11:10:10 +0800

    | 8.4 | ⚡ Async Model Execution (AI/ML/Processing Simulation)                | Run dummy AI logic inside route using await to simulate long image processing |
    | 8.4 | ⚡ Async Model Execution (AI/ML/Processing Simulation)                | Run dummy AI logic inside route using await to simulate long image processing |

--- Flask/tutorial_flask/topic_8.4/app.py
+++ Flask/tutorial_flask/topic_8.4/app.py
@@ -8,7 +8,7 @@ from routes.posts import posts_bp
 from routes.gets import gets_bp
 from routes.deletes import deletes_bp
 from routes.user_routes import user_bp
-
+from routes.ai_routes import ai_bp
 #Load variables from .env file
 load_dotenv()
 
@@ -24,7 +24,7 @@ def create_app():
     app.register_blueprint(gets_bp,url_prefix="/gets")
     app.register_blueprint(deletes_bp,url_prefix="/deletes")
     app.register_blueprint(user_bp,url_prefix="/users")
-        
+    app.register_blueprint(ai_bp,url_prefix="/ai")   
     return app
       
 
