07f3a18df80e87989d17a55622767a9e9b30d258
Abdul Qayyum Isahak <khoi00091@gmail.com>
2025-04-22 22:00:29 +0800

    | 1.3 | 🧱 Initialize Database Connection | Configure `DATABASE_URL`, setup engine, session, and `Base` class |
    | 1.3 | 🧱 Initialize Database Connection | Configure `DATABASE_URL`, setup engine, session, and `Base` class |

---
+++ Database/learning_1/db/database.py
@@ -0,0 +1,22 @@
+#db/database.py
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker,AsyncSession
+from sqlalchemy.orm import declarative_base
+import os
+
+
+#load .env
+load_dotenv()
+#connection string
+DATABASE_URL = os.getenv("DATABASE_URL")
+#engine - connection to db (postgreSQL)
+engine = create_async_engine(DATABASE_URL,echo=True)
+#session - init session for query
+sessionLocal = async_sessionmaker(bind=engine,class_=AsyncSession,expire_on_commit=False)
+#Base = model/table
+base =  declarative_base()
+
+async def get_db():
+    async with sessionLocal() as session:
+        yield session
